f497407738e4b1e198d50bb785e5ab69fe3642c6
Andres Martinez <andresfelipemartinezt@gmail.com>
2024-08-23 22:08:05 -0500

    feature/input installing all dependecies

--- frontend/src/components/home/Home.tsx
+++ frontend/src/components/home/Home.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import { FormProvider, useForm } from "react-hook-form";
+import Input from "../input/Input";
+import { userSchema } from "../schemas/user";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Style from '../../styles/Input.module.css'
 
-type Props = {}
+// type Props = {}
 
-const Home = (props: Props) => {
+const Home = () => {
+const methods = useForm({
+  resolver: zodResolver(userSchema)
+})
   return (
-    <div>Home</div>
-  )
-}
+    <div className={Style.l_form}>
+      <FormProvider {...methods}>
+        <form className={Style.form}>
+          <h1 className={Style.form_title}>Iniciar Sesión</h1>
+          <Input type="email" name="email">
+            Email
+          </Input>
+          <Input type="password" name="password">
+            Password
+          </Input>
+          <button className={Style.form_button}>Enviar</button>
+        </form>
+      </FormProvider>
+      <div className={Style.pelo}>
+        <FormProvider {...methods} >
+          <form className={Style.pelo_form}>
+            <Input type="text" name='pelo'>Pelo</Input>
+          </form>
+        </FormProvider>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
