c84fc15c20d9220c3a4a368a344e405174abdb4a
Andres Martinez <andresfelipemartinezt@gmail.com>
2024-08-28 19:38:25 -0500

    feature/formUser: creating schema users to validate

--- frontend/src/components/schemas/user.tsx
+++ frontend/src/components/schemas/user.tsx
@@ -1,22 +1,95 @@
-import { z } from 'zod';
+import { z } from "zod";
 
-export const userSchema = z.object({
-  name: z
-  .string()
-  .min(1, { message: "Este campo es requerido" })
-  .min(3, { message: "Debe de contener mínimo 3 caracteres"}),
-  lastname: z
-  .string()
-  .min(1, { message: "Este campo es requerido" })
-  .min(3, { message: "Debe de contener mínimo 3 caracteres"}),
+export const documentType = [
+  "Cédula de ciudadanía",
+  "Cédula de extranjería",
+  "NIT",
+] as const;
 
-  email: z
-  .string()
-  .min(1, { message: "Este campo es requerido" })
-  .email("Debe ser un correo electrónico"),
+const identificationValidator = z.union([
+  z.object({
+    type: z.literal("Cédula de ciudadanía"),
+    identification: z
+      .string()
+      .regex(/^\d+$/, "Cédula de ciudadanía sólo puede contener números."),
+  }),
+  z.object({
+    type: z.literal("Cédula de extranjería"),
+    identification: z
+      .string()
+      .regex(
+        /^[A-Za-z0-9]+$/,
+        "Hubo un error en el tipado, inténtelo de nuevo."
+      ),
+  }),
+  z.object({
+    type: z.literal("NIT"),
+    identification: z
+      .string()
+      .regex(
+        /^\d{3}\.\d{3}\.\d{3}-\d$/,
+        "NIT debe de seguir el formato de 123.456.789-0."
+      ),
+  }),
+]);
 
-  password: z
-  .string()
-  .min(1, { message: "Este campo es requerido" }),
+export const userSchema = z
+  .object({
+    name: z
+      .string()
+      .min(1, { message: "Este campo es requerido." })
+      .min(3, { message: "Debe de contener mínimo 3 caracteres." })
+      .max(20, { message: "Debe contener máximo 20 caracteres." }),
+    lastname: z
+      .string()
+      .regex(/^\d+$/, { message: "Solo puede contener números" })
+      .min(1, { message: "Este campo es requerido." })
+      .min(7, { message: "Debe de contener mínimo 7 dígitos." })
+      .max(10, { message: "Debe contener máximo 10 dígitos." }),
+    type: z.enum(documentType),
+    identification: z.string(),
+    phone: z
+      .string()
+      .min(1, { message: "Este campo es requerido." })
+      .min(7, { message: "Debe de contener al menos 7 digitos." })
+      .max(10, { message: "Máximo puede contener 10 dígitos." }),
+    email: z
+      .string()
+      .min(1, { message: "Este campo es requerido" })
+      .email("Debe ser un correo electrónico")
+      .regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, {
+        message: "Formato del correo es inválido",
+      })
+      .max(20, { message: "Debe contener máximo 20 caracteres." }),
+    password: z
+      .string()
+      .min(1, { message: "Este campo es requerido." })
+      .max(50, { message: "Debe contener máximo 50 caracteres." })
+      .regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/, {
+        message:
+          "La contraseña debe de  contener al menos 8 caracteres, 1 letra minúscula, 1 letra mayúscula, un número y puede contener caracteres especiales.",
+      }),
+    address: z.string().optional(),
+    city: z.string().optional(),
+    otherContact: z.string().optional(),
+    otherPhone: z
+     .string()
+     .regex(/^\d+$/, { message: "Solamente puede contener números" })
+     .optional(),
+  })
+  .refine(
+    (id) => {
+      const { type, identification } = id;
+      const result = identificationValidator.safeParse({
+        type,
+        identification,
+      });
+      return result.success;
+    },
+    {
+      message: "El formato de identificación no es válido.",
+      path: ["identification"],
+    }
+  );
 
-})
+type DataUserForm = z.infer<typeof userSchema>;
